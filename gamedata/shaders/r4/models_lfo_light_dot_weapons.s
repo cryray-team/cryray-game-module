65f4f8fc470d9e39e76a5aded5e0c5d05ff14ccb
OldSerpskiStalker <ictopiar@gmail.com>
2023-10-20 23:45:32 +0500

    [Shaders]: Fix skin.h

--- gamedata/shaders/r4/models_lfo_light_dot_weapons.s
+++ gamedata/shaders/r4/models_lfo_light_dot_weapons.s
@@ -1,13 +1,3 @@
---' //////////////////////////////////////////////////////////////////////////
---' //-' CryRay Engine x64 Shaders
---' //-' Based on shaders from the original Anomaly 1.5.1
---' //-' As well as an integrated shader pack Screen Space 15.4
---' //-' Repository with shaders: https://github.com/cryray-team
---' //-' Also, if you are a shader developer
---' //-' You can join our server in discord: https://discord.gg/R78uVUZCTR
---' //-' OldSerpskiStalker7777, CryRay Team
---' //////////////////////////////////////////////////////////////////////////
-
 function normal(shader, t_base, t_second, t_detail)
 	  shader:begin  ("model_def_lplanes","models_lfo_light_dot_weapons")
       : fog       	(true)
